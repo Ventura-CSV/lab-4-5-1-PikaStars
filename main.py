5d70feae9272a9ee818745f74e503744dab46180
PikaStars <pikachustarsza@gmail.com>
2024-06-28 22:28:23 -0700

    commit1

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    numbers = []
+    total = 0
+    count = 0
+    while (count < 5):
+        count = count + 1
+        num = random.randrange(0, 100)
+        numbers.append[num]
+        print(num) 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
